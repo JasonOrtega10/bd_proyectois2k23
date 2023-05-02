ada992923688048c55974b12352d0ca4614b0308
leoneldc <leonelchavez2013@hotmail.com>
2023-05-02 09:01:11 -0600

    changes tables in cxc

--- Modulos/Administración/Cuentas por Cobrar/script.sql
+++ Modulos/Administración/Cuentas por Cobrar/script.sql
@@ -13,26 +13,24 @@ CREATE TABLE if not exists `tbl_TipoPago` (
 	nombre_tipopago VARCHAR(50) not null,
 	estado_tipopago tinyint not null
 );
-CREATE TABLE if not exists `tbl_BalanceClientes` (
-	fk_id_cliente INT PRIMARY KEY auto_increment,   
-    saldo_mes_anterior FLOAT DEFAULT 0
-    -- saldo actual, 
-    -- cargo del mes, 
-    -- abonos del mes,  
-    -- cargos acumulados,
-    -- abonos acumulados,
-    -- mas campos por determinar xd
-    -- FOREIGN KEY (fk_id_cliente) REFERENCES tbl_Cliente(id)
+CREATE TABLE if not exists `tbl_Mora` (
+	pk_id_mora INT PRIMARY KEY auto_increment,
+	mes_min_mora FLOAT not null,
+	mes_max_mora FLOAT not null,
+	cantidad_fija_mora FLOAT not null,
+	cantidad_porcentaje_mora FLOAT not null,
+	estado_tipopago tinyint not null
 );
 CREATE TABLE if not exists `tbl_CuentaPorCobrar`(
   id INT PRIMARY KEY auto_increment,
   fk_id_factura INT not null,
-  pk_id_tipopago INT not null,
-  pk_id_moneda INT not null,
-  -- mora
+  fk_id_tipoPago INT not null,
+  fk_id_moneda INT not null,
+  fk_id_mora INT not null,
   fecha_pago DATE not null,
-  abonado_pago float not null
+  abonado_pago float not null,
   -- FOREIGN KEY (fk_id_factura) REFERENCES EncabezadoFactura(id_encabezado),
-  -- FOREIGN KEY (fk_id_tipoPago) REFERENCES tbl_TipoPago(pk_id_tipopago),
-  -- FOREIGN KEY (fk_id_moneda) REFERENCES tbl_Moneda(pk_id_moneda)
+  FOREIGN KEY (fk_id_tipoPago) REFERENCES tbl_TipoPago(pk_id_tipopago),
+  FOREIGN KEY (fk_id_moneda) REFERENCES tbl_Moneda(pk_id_moneda),
+  FOREIGN KEY (fk_id_mora) REFERENCES tbl_Mora(pk_id_mora)
 );
