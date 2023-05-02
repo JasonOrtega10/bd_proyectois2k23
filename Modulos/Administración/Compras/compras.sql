8035232f7e7ae66223ec95d4a7d84d4d356f816d
Mario López <mariodlopez17@gmail.com>
2023-05-02 09:12:13 -0600

    Cambios bd_compras

--- Modulos/Administración/Compras/compras.sql
+++ Modulos/Administración/Compras/compras.sql
@@ -1,43 +1,6 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
---
--- Host: localhost    Database: compramod
--- ------------------------------------------------------
--- Server version	8.0.28
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `compras`
---
-
-DROP TABLE IF EXISTS `tbl_proveedor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tbl_proveedor`(
-pk_id_proveedor int not null,
-nombre_proveedor varchar(50) not null,
-direccion_proveedor varchar(50) not null,
-telefono_proveedor int not null,
-correo_proveedor varchar(50) not null,
-rubro_proveedor varchar(100) not null,
-estado_proveedor tinyint not null default 0,
-primary key(pk_id_proveedor)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 
 DROP TABLE IF EXISTS `compras`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `compras` (
   `id_compras` int NOT NULL,
   `id_detalle_ordendecompras` int DEFAULT NULL,
@@ -45,24 +8,17 @@ CREATE TABLE `compras` (
   KEY `id_detalleordencompras_idx` (`id_detalle_ordendecompras`),
   CONSTRAINT `id_detalleordencompras` FOREIGN KEY (`id_detalle_ordendecompras`) REFERENCES `detalle_orden_de_compra` (`iddetalle_orden_de_compra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `compras`
---
+
+
 
 LOCK TABLES `compras` WRITE;
-/*!40000 ALTER TABLE `compras` DISABLE KEYS */;
-/*!40000 ALTER TABLE `compras` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `detalle_compras`
---
 
 DROP TABLE IF EXISTS `detalle_compras`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `detalle_compras` (
   `id_detalle_compras` int NOT NULL,
   `id_compras` int DEFAULT NULL,
@@ -71,86 +27,68 @@ CREATE TABLE `detalle_compras` (
   KEY `idcomprass_idx` (`id_compras`),
   CONSTRAINT `idcomprass` FOREIGN KEY (`id_compras`) REFERENCES `compras` (`id_compras`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `detalle_compras`
---
+
 
 LOCK TABLES `detalle_compras` WRITE;
-/*!40000 ALTER TABLE `detalle_compras` DISABLE KEYS */;
-/*!40000 ALTER TABLE `detalle_compras` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `detalle_factura`
---
+
 
 DROP TABLE IF EXISTS `detalle_factura`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `detalle_factura` (
   `iddetalle_factura` int NOT NULL AUTO_INCREMENT,
   `id_factura` int DEFAULT NULL,
   `nit` int DEFAULT NULL,
   `fecha_creacion` varchar(45) DEFAULT NULL,
   `nombre_encargado` varchar(45) DEFAULT NULL,
-  `estado` varchar(45) DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`iddetalle_factura`),
   KEY `id_factura_idx` (`id_factura`),
   CONSTRAINT `idfactura` FOREIGN KEY (`id_factura`) REFERENCES `factura` (`id_factura`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `detalle_factura`
---
+
+
 
 LOCK TABLES `detalle_factura` WRITE;
-/*!40000 ALTER TABLE `detalle_factura` DISABLE KEYS */;
-/*!40000 ALTER TABLE `detalle_factura` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `detalle_orden_de_compra`
---
+
 
 DROP TABLE IF EXISTS `detalle_orden_de_compra`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `detalle_orden_de_compra` (
   `iddetalle_orden_de_compra` int NOT NULL,
   `id_orden_de_compra` int DEFAULT NULL,
+  `fk_id_proveedor` int DEFAULT NULL,
   `id_producto` int DEFAULT NULL,
-  `cantidad` varchar(45) DEFAULT NULL,
-  `total` varchar(45) DEFAULT NULL,
-  `estado` varchar(45) DEFAULT NULL,
-  `fk_id_proveedor` INT,
+  `cantidad` int DEFAULT NULL,
+  `total` float DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`iddetalle_orden_de_compra`),
   KEY `id_orden_de_compra_idx` (`id_orden_de_compra`),
   KEY `id_producto_idx` (`id_producto`),
+  KEY `fk_id_proveedor` (`fk_id_proveedor`),
+  CONSTRAINT `fk_id_proveedor` FOREIGN KEY (`fk_id_proveedor`) REFERENCES `tbl_proveedor` (`pk_id_proveedor`),
   CONSTRAINT `id_orden_de_compra` FOREIGN KEY (`id_orden_de_compra`) REFERENCES `orden_de_compra` (`id_orden_de_compra`),
-  CONSTRAINT `fk_id_proveedor` FOREIGN KEY (`fk_id_proveedor`) REFERENCES `tbl_Proveedor` (`pk_id_proveedor`),
   CONSTRAINT `id_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_productos`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `detalle_orden_de_compra`
---
+
+
 
 LOCK TABLES `detalle_orden_de_compra` WRITE;
-/*!40000 ALTER TABLE `detalle_orden_de_compra` DISABLE KEYS */;
-/*!40000 ALTER TABLE `detalle_orden_de_compra` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `devolucion`
---
+
 
 DROP TABLE IF EXISTS `devolucion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `devolucion` (
   `id_devolucion` int NOT NULL AUTO_INCREMENT,
   `id_detalle_factura` int DEFAULT NULL,
@@ -159,51 +97,39 @@ CREATE TABLE `devolucion` (
   KEY `id_detallefactura_idx` (`id_detalle_factura`),
   CONSTRAINT `id_detallefactura` FOREIGN KEY (`id_detalle_factura`) REFERENCES `detalle_factura` (`iddetalle_factura`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `devolucion`
---
+
+
 
 LOCK TABLES `devolucion` WRITE;
-/*!40000 ALTER TABLE `devolucion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `devolucion` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `factura`
---
+
 
 DROP TABLE IF EXISTS `factura`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `factura` (
   `id_factura` int NOT NULL AUTO_INCREMENT,
   `id_compras` int DEFAULT NULL,
-  `total` FLOAT,
-  `estatus` TINYINT,
+  `total` float DEFAULT NULL,
+  `estatus` tinyint DEFAULT NULL,
   PRIMARY KEY (`id_factura`),
   KEY `id_compras_idx` (`id_compras`),
   CONSTRAINT `id_compras` FOREIGN KEY (`id_compras`) REFERENCES `compras` (`id_compras`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `factura`
---
+
+
 
 LOCK TABLES `factura` WRITE;
-/*!40000 ALTER TABLE `factura` DISABLE KEYS */;
-/*!40000 ALTER TABLE `factura` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `orden_de_compra`
---
+
 
 DROP TABLE IF EXISTS `orden_de_compra`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `orden_de_compra` (
   `id_orden_de_compra` int NOT NULL AUTO_INCREMENT,
   `fecha_creacion_orden` varchar(45) DEFAULT NULL,
@@ -212,48 +138,53 @@ CREATE TABLE `orden_de_compra` (
   `fecha_entre_orden_compra` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_orden_de_compra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `orden_de_compra`
---
+
 
 LOCK TABLES `orden_de_compra` WRITE;
-/*!40000 ALTER TABLE `orden_de_compra` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orden_de_compra` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `productos`
---
+
 
 DROP TABLE IF EXISTS `productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `productos` (
   `id_productos` int NOT NULL,
   `producto` varchar(45) DEFAULT NULL,
-  `precio` varchar(45) DEFAULT NULL,
+  `marca` varchar(45) DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  `precio_unitario` float DEFAULT NULL,
   PRIMARY KEY (`id_productos`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `productos`
---
+
+
 
 LOCK TABLES `productos` WRITE;
-/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-27 15:46:10
+
+DROP TABLE IF EXISTS `tbl_proveedor`;
+CREATE TABLE `tbl_proveedor` (
+  `pk_id_proveedor` int NOT NULL,
+  `nombre_proveedor` varchar(50) NOT NULL,
+  `direccion_proveedor` varchar(50) NOT NULL,
+  `telefono_proveedor` int NOT NULL,
+  `correo_proveedor` varchar(50) NOT NULL,
+  `rubro_proveedor` varchar(100) NOT NULL,
+  `estado_proveedor` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`pk_id_proveedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+--
+-- Dumping data for table `tbl_proveedor`
+--
+
+LOCK TABLES `tbl_proveedor` WRITE;
+
+UNLOCK TABLES;
+
+
