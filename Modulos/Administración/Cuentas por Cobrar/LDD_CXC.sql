4b2f03e44af1444df3062214cebb226e98263181
leoneldc <leonelchavez2013@hotmail.com>
2023-05-08 16:40:24 -0600

    complete all tables for cxc

--- Modulos/Administración/Cuentas por Cobrar/LDD_CXC.sql
+++ Modulos/Administración/Cuentas por Cobrar/LDD_CXC.sql
@@ -15,7 +15,7 @@ CREATE TABLE if not exists `tbl_TipoPago` (
 );
 CREATE TABLE if not exists `tbl_CuentaPorCobrar`(
   fk_id_factura INT not null primary key,
-  fk_id_almacen INT not null key,
+  fk_id_almacen INT not null,
   fk_id_tipoPago INT not null,
   fk_id_moneda INT not null,
   fecha_emision_cxc DATE not null,
@@ -24,7 +24,8 @@ CREATE TABLE if not exists `tbl_CuentaPorCobrar`(
   monto_cobro_cxc float not null default 0,
   monto_pago_cxc float not null default 0,
   estado_cxc tinyint default 0,
-  -- FOREIGN KEY (fk_id_factura) REFERENCES EncabezadoFactura(id_encabezado),
+  key(fk_id_almacen),
+  FOREIGN KEY (fk_id_factura) REFERENCES tbl_venta(Pk_idVenta),
   -- FOREIGN KEY (fk_id_almacen) REFERENCES tbl_Almacen(pk_id_almacen),
   FOREIGN KEY (fk_id_tipoPago) REFERENCES tbl_TipoPago(pk_id_tipopago),
   FOREIGN KEY (fk_id_moneda) REFERENCES tbl_Moneda(pk_id_moneda)
