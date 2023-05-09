091c3ed1e04f5c1911b12d88645a251ec62e0ad8
LuisTorres2122 <ltorresb@miumg.edu.gt>
2023-05-09 15:52:58 -0600

    Ultima modificacion script cxp

--- Modulos/Administración/Cuentas por Pagar/LDD_CXP.sql
+++ Modulos/Administración/Cuentas por Pagar/LDD_CXP.sql
@@ -19,6 +19,13 @@ estado_proveedor tinyint not null,
 primary key(pk_id_proveedor)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE tbl_almacen
+(
+	codigo_almacen INT PRIMARY KEY NOT NULL,
+    nombre_almacen VARCHAR(60) NOT NULL,
+    estatus_almacen VARCHAR(1) NOT NULL
+) ENGINE=INNODB DEFAULT CHARSET=latin1;
+
 create table if not exists `tbl_Moneda`(
 	pk_id_moneda int primary key auto_increment,
 	nombre_moneda VARCHAR(50) not null,
@@ -41,28 +48,25 @@ estado_conceptocuentaporpagar tinyint default 0 not null,
 primary key(pk_id_conceptocuentaporpagar)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*CREATE TABLE if not exists `factura` (
-  `id_factura` int NOT NULL AUTO_INCREMENT,
-  `id_compras` int DEFAULT NULL,
-  `total` FLOAT,
-  `estatus` TINYINT,
-  PRIMARY KEY (`id_factura`),
-  KEY `id_compras_idx` (`id_compras`),
-  CONSTRAINT `id_compras` FOREIGN KEY (`id_compras`) REFERENCES `compras` (`id_compras`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;*/
-
-CREATE TABLE if not exists `factura` (
-`id_factura` int not null,
-primary key(id_factura)
+CREATE TABLE `tbl_factura` (
+  pk_id_factura int NOT NULL,
+  pk_id_almacen INT not null,
+  pk_id_proveedor INT not null,
+  fecha_emision_factura DATE not null,
+  total_factura float DEFAULT NULL,
+  estatus_factura tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (pk_id_factura,pk_id_almacen,pk_id_proveedor),
+  FOREIGN KEY (pk_id_almacen) REFERENCES tbl_almacen(codigo_almacen),
+  FOREIGN KEY (pk_id_proveedor) REFERENCES tbl_Proveedor(pk_id_proveedor)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE if not exists `tbl_CuentaPorPagar`(
-  pk_id_cuentaporpagar Int PRIMARY KEY,
-  -- fk_id_almacen INT not null PRIMARY KEY,
-  fk_id_proveedor INT not null,
+  pk_id_cuentaporpagar INT not null,
+  pk_id_almacen INT not null ,
+  pk_id_proveedor INT not null,
+  pk_id_factura INT not null,
   fk_id_tipopago INT not null,
   fk_id_moneda INT not null,
-  fk_id_factura INT not null,
   fk_id_conceptocuentaporpagar INT not null,
   cambio_moneda_cuentaporpagar float not null,
   fecha_emision_cuentaporpagar DATE not null,
@@ -70,10 +74,11 @@ CREATE TABLE if not exists `tbl_CuentaPorPagar`(
   saldo_pago_cuentaporpagar float not null,
   monto_pago_cuentaporpagar float not null,
   estado_cuentaporpagar tinyint default 0,
+  PRIMARY KEY (pk_id_cuentaporpagar,pk_id_almacen,pk_id_proveedor,pk_id_factura),
   FOREIGN KEY (fk_id_tipopago) REFERENCES tbl_TipoPago(pk_id_tipopago),
-  -- FOREIGN KEY (fk_id_almacen) REFERENCES tbl_Almacen(pk_id_almacen),
-  FOREIGN KEY (fk_id_proveedor) REFERENCES tbl_Proveedor(pk_id_proveedor),
+  FOREIGN KEY (pk_id_almacen) REFERENCES tbl_almacen(codigo_almacen),
+  FOREIGN KEY (pk_id_proveedor) REFERENCES tbl_Proveedor(pk_id_proveedor),
   FOREIGN KEY (fk_id_moneda) REFERENCES tbl_Moneda(pk_id_moneda),
   FOREIGN KEY (fk_id_conceptocuentaporpagar) REFERENCES tbl_ConceptoCuentaPorPagar(pk_id_conceptocuentaporpagar),
-  FOREIGN KEY (fk_id_factura) REFERENCES factura(id_factura)
+  FOREIGN KEY (pk_id_factura) REFERENCES tbl_factura(pk_id_factura)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
